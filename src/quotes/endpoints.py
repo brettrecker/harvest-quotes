b735fbe698556102174942e5f1e0ad80d1bc9469
brettrecker <reckerb5@gmail.com>
2024-01-26 16:03:36 -0600

    add endpoint configuration and freeze requirements

--- src/quotes/endpoints.py
+++ src/quotes/endpoints.py
@@ -13,7 +13,27 @@ class HQEndpoints:
         self.router.add_api_route("/",
                                   self.process_message,
                                   methods=["POST"],
-                                  summary="Testing")
+                                  summary="Root")
+
+        self.router.add_api_route("/quotes",
+                                  self.process_message,
+                                  methods=["POST", "PATCH", "PUT", "GET"],
+                                  summary="Quotes")
+
+        self.router.add_api_route("/customers",
+                                  self.process_message,
+                                  methods=["POST", "PATCH", "PUT", "GET"],
+                                  summary="Customers")
+
+        self.router.add_api_route("/users",
+                                  self.process_message,
+                                  methods=["POST", "PATCH", "PUT", "GET"],
+                                  summary="Users")
+
+        self.router.add_api_route("/companies",
+                                  self.process_message,
+                                  methods=["POST", "PATCH", "PUT", "GET"],
+                                  summary="Companies")
 
     async def process_message(self):
         return 'test'
